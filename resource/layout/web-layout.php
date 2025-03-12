d1df7cae8027dcdf466a2b7e1e6cd4352c18cb12
eli <e.sorono.544027@umindanao.edu.ph>
2025-03-12 18:39:41 +0800

    updated

--- resource/layout/web-layout.php
+++ resource/layout/web-layout.php
@@ -18,7 +18,7 @@ unset($_SESSION['login']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Inventory</title>
     <!-- css -->
     <link rel="stylesheet" href="../css/dashboard.css">
     <link rel="stylesheet" href="../css/inventory.css">
